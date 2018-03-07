25fca83f1832dda8ea2b22ff2ec67aed04037952
raikaat <raikaat@gmail.com>
2018-03-07 23:35:53 +0300

    add index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function longestConsecutiveLength(array) {
+  let result = [];
+  let set = new Set(array);
+
+    for(let item of set){
+      if(!set.has(item-1)){
+        let count = 1;
+
+        while(set.has(item+1)){
+          count++;
+          item++;
+        }
+        result.push(count);
+      }
+    }
+  return (result[0]) ? Math.max.apply(null, result) : 0;
   // your solution here
 }
